f7417150fc59113cc133dbf76692ff574f4cace9
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-01-16 20:50:24 +0100

    starting cpp

---
+++ test_write.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int	main()
+{
+	int	n = 42;
+	char	c = 'c';
+	char	str[12] = "Hello word!";
+	double	x = 3.14159265358979; // n'affiche pas apres la 5eme decimale normal
+
+	cout << "Valeur de n: " << n << "\n";
+	cout << "Valeur de c: " << c << "\n";
+	cout << "Valeur de str: " << str << "\n";
+	cout << "Valeur de x: " << x << "\n";
+	return 0;
+}
+
